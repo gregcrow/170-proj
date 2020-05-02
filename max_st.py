5a7d78db26ee012bcbc331370d0c733804a6b305
Gregory Crow <gregorycrow@berkeley.edu>
2020-05-02 09:41:33 -0700

    Now all the inputs run directly from running solver.py, max_st.py now goes back to its original function to return the MAX_spanning_tree for all inputs

--- max_st.py
+++ max_st.py
@@ -1,6 +1,5 @@
 from parse import *
 import networkx as nx
-from solver import solve
 import os
 
 if __name__ == "__main__":
@@ -8,7 +7,6 @@ if __name__ == "__main__":
     input_dir = "inputs"
     for input_path in os.listdir(input_dir):
         graph_name = input_path.split(".")[0]
-        print(input_path)
         G = read_input_file(f"{input_dir}/{input_path}")
-        T = solve(G)
+        T = nx.maximum_spanning_tree(G)
         write_output_file(T, f"{output_dir}/{graph_name}.out")
--- solver.py
+++ solver.py
@@ -1,8 +1,9 @@
 import networkx as nx
 from networkx.algorithms import approximation as ax
-from parse import read_input_file, write_output_file
+from parse import *
 from utils import is_valid_network, average_pairwise_distance, average_pairwise_distance_fast
 import sys
+import os
 
 
 def solve(G):
@@ -87,16 +88,11 @@ def reverse_prune(G, T):
 
 # Usage: python3 solver.py test.in
 
-if __name__ == '__main__':
-    assert len(sys.argv) == 2
-    path = sys.argv[1]
-    G = read_input_file(path)
-    T = solve(G)
-    # T_prime = T.copy()
-    # T_prime.remove_edges_from([(32,33)])
-    # T_prime.remove_node(32)
-    # print(average_pairwise_distance_fast(T_prime))
-    assert is_valid_network(G, T)
-    print("Average  pairwise distance: {}".format(average_pairwise_distance_fast(T)))
-    print(average_pairwise_distance_fast(nx.maximum_spanning_tree(G)))
-    write_output_file(T, 'out/test.out')
+if __name__ == "__main__":
+    output_dir = "submission"
+    input_dir = "inputs"
+    for input_path in os.listdir(input_dir):
+        graph_name = input_path.split(".")[0]
+        G = read_input_file(f"{input_dir}/{input_path}")
+        T = solve(G)
+        write_output_file(T, f"{output_dir}/{graph_name}.out")
